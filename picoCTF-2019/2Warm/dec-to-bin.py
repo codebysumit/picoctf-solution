7180a6b77e7b9c5933b49d297365d9cf40942cb1
Sumit Das <87431704+codebysumit@users.noreply.github.com>
2025-08-31 18:37:28 +0530

    Create dec-to-bin.py

---
+++ picoCTF-2019/2Warm/dec-to-bin.py
@@ -0,0 +1,25 @@
+# https://play.picoctf.org/practice/challenge/86
+
+'''
+decimal_number = 42
+binary_representation = bin(decimal_number)
+print(binary_representation)
+'''
+
+dec = 42
+bin = []
+
+while dec > 0:
+    bin.append(dec % 2)
+    dec = dec // 2
+
+# Reverse the list to get the correct binary representation
+bin.reverse()
+
+# print(len(bin))
+# print(bin)
+
+length = len(bin)
+for i in range(length):
+    print(bin[i], end="")
+  
