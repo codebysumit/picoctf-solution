3cc7f7d6969f80e838fd20be44eeabe48b91eb95
Sumit Das <87431704+codebysumit@users.noreply.github.com>
2025-08-31 22:05:27 +0530

    Create hex_to_dec.py

---
+++ picoCTF-2019/Warmed Up/hex_to_dec.py
@@ -0,0 +1,7 @@
+# https://play.picoctf.org/practice/challenge/58
+
+hex_literal = hex(0x3D)
+decimal_value = int(hex_literal, 16)
+print(f"The hexadecimal value '{hex_literal}' is equal to the decimal value: {decimal_value}")
+
+# picoCTF{61}
